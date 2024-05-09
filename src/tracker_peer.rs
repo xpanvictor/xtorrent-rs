27b8965fdbcc3d38cd34f5c4ad46917d7faaa7a7
xpanvictor <xpanvictor@gmail.com>
2024-05-09 23:11:56 +0100

    doc(tracker): works on the tracker methodology

--- src/tracker_peer.rs
+++ src/tracker_peer.rs
@@ -1,10 +1,19 @@
+use std::collections::HashMap;
+
+use reqwest::Url;
+
 use crate::torrent_spec::TorrentMeta;
 
 pub struct Peers {}
 
 impl Peers {
     pub fn retrieve_peers(&mut self, t_info: TorrentMeta) -> Peers {
-        let peer_data = reqwest::get(t_info.announce).form();
+        let peer_data_fetch_url =
+            Url::parse_with_params(&t_info.announce, Self::generate_params_for_peers(&t_info));
         Peers {}
+    }
+
+    fn generate_params_for_peers(t_info: &TorrentMeta) -> HashMap<String, String> {
+        todo!()
     }
 }
